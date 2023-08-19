6b6f5761c8c0f11129ad6a38a526bde236edfac7
Yahya Sungur <yahya.sungur@codevo.com.tr>
2023-08-19 20:13:55 +0300

    middleware functionality constructed.

---
+++ src/middleware/validateResource.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import { AnyZodObject } from 'zod';
+
+const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
+    try{
+        schema.parse({
+            body: req.body,
+            query: req.query,
+            params: req.params
+        });
+    } catch (error: any) {
+        return res.status(400).json({ error: error.errors });
+    }
+};
+
+export default validate;
